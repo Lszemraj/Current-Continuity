cb011883085769c54f4bdb70a2b74e3b0dc42406
Lszemraj <lillieszemraj@gmail.com>
2022-04-11 10:25:55 -0500

    recent modifications

--- ContinuityV2.py
+++ ContinuityV2.py
@@ -6,13 +6,11 @@ df_dict = load_all_geoms()
 generate_dict, conductor_dict, id_column_dict = make_conductor_dicts(df_dict)
 
 
-
-
 # these tell us which index corresponds to which conductor key
 sorted_dict = sorted(conductor_dict.items())
-print(sorted_dict)
+#print(sorted_dict)
 index_dict = {i:k[0] for i,k in enumerate(sorted_dict)} # save this
-print(index_dict) #First num is index in matrix, second num is conductor index
+#print(index_dict) #First num is index in matrix, second num is conductor index
 
 
 datadir = '/home/shared_data/helicalc_params/'
@@ -33,58 +31,63 @@ with open(connected_file, 'rb') as file:
 with open(close_file, 'rb') as file:
     close_matrix = pkl.load(file)
 
-print(connected_matrix)
-print(connected_matrix[0])
-print("loc", np.argwhere(connected_array[0]))
-print(connected_array[0][40])
-print(np.amin(min_dist_matrix[0]))
-print(np.where(min_dist_matrix[0] == np.amin(min_dist_matrix[0]) ))
+
+
 connection_dict = {}
 
-def find_connection(index_dict, min_dist_matrix, connected_array):
+def find_neighbour(index_dict, min_dist_matrix, connected_array):
     for key, value in enumerate(index_dict):
         index_number = key
         conductor_num = value
-        x = int(np.argwhere(connected_matrix[index_number]))
-        if x:
+        try:
+            x = int(np.argwhere(connected_matrix[index_number]))
             connected_conductor = index_dict[x]
             connection_dict[value] = connected_conductor
-        else:
-            closest = np.where(min_dist_matrix[key] == np.amin(min_dist_matrix[key]) )
+        except:
+            closest = np.argwhere(min_dist_matrix[key] == np.amin(min_dist_matrix[key]) )
             closest_conductor = index_dict[int(closest)]
             connection_dict[value] = closest_conductor
 
 
-find_connection(index_dict, min_dist_matrix, connected_matrix)
-print(connection_dict)
+reverse_index_dict = {k[0]:i for i,k in enumerate(sorted_dict)}
+
+def find_correct_order(reverse_index_dict, index_dict, close_matrix):
+    conductor_chain = [index_dict[0]]
+    conductors_checked = [index_dict[0]]
+    conductor_list_chain = [] #store diff chains
+    for cond_num in range(0, len(close_matrix)): #81 total conductors
+        try:
+            initial_cond = reverse_index_dict[conductor_chain[-1]]
+            closest = np.argwhere(close_matrix[initial_cond] == True)
+            closest_conductor = index_dict[int(closest)]
+            conductor_chain.append(closest_conductor)
+            conductors_checked.append(closest_conductor)
+        except:
+            try:
+                last_cond = reverse_index_dict[conductor_chain[0]]
+                closest = np.argwhere(close_matrix[:, last_cond] == True)
+                closest_conductor = index_dict[int(closest)]
+                conductor_chain.insert(0, closest_conductor)
+                conductors_checked.append(closest_conductor)
+            except: #chain is finished
+                conductor_list_chain.append(conductor_chain)
+                for i in reverse_index_dict.keys():
+                    if i not in conductors_checked:
+                        conductor_chain = [i]
+                        conductors_checked.append(i)
+                        break
+    conductor_list_chain.append(conductor_chain)
+    return conductor_list_chain, conductors_checked
 
 
 
 
 
 
-#print("minimum distance natrix", min_dist_matrix)
-#print("connected matrix", connected_matrix)
-#print("close_matrix", close_matrix)
-
-connections = np.sum(connected_matrix, axis = 0)
-#print(connections)
-#print(len(connections))
-
-connections2 = np.sum(connected_matrix, axis = 1)
-#print(connections2)
-#print(len(connections2))
-
-unconnected_coils = []
-#unconnected_coils_output_input = []
-
-for i in range(0, len(connections)):
-    if connections[i] == 0:
-        #unconnected_coils_output_input.append(output_input[i])
-        unconnected_coils.append(i+1)
-
-#print(unconnected_coils)
-output_input = np.argwhere(connections)
-#print(output_input)
-
-#def put_in_order(min_dist, coil_list):
+x, y = find_correct_order(reverse_index_dict, index_dict, close_matrix)
+print("conductor list chain:", x)
+print("conductors checked:", y)
+print("conductors not in conductors_checked:")
+for i in reverse_index_dict.keys():
+    if i not in y:
+        print(i)
